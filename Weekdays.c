25df992c4699d4721510e3f781478c153521b87d
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-04 23:11:48 +0530

    Add files via upload

---
+++ Weekdays.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("Sunday");
+	printf("\nMonday");
+	printf("\nTuesday")
+	printf("\nWednesday");
+	printf("\nThursday");
+	printf("\nFriday");
+	printf("\nSaturday");
+}
